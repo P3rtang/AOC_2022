d992b678a377807548906c0da0ca788308dc766f
P3rtang <tomverhulst21@gmail.com>
2022-12-05 21:29:38 +0100

    reformatted to show in overview

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -3,10 +3,43 @@ use std::io::Read;
 
 pub struct Game {
     state: (String, String),
-    points: (i32, i32),
 }
 
-pub fn parse_input(file_path: &str, win_state: bool) -> Vec<Game> {
+impl Game {
+    fn as_str(&self) -> (&str, &str) {
+        return (self.state.0.as_str(), self.state.1.as_str())
+    }
+    fn get_points_1(&self) -> (i32, i32) {
+        return match self.as_str() {
+            ("A", "X") => (4, 4),
+            ("A", "Y") => (1, 8),
+            ("A", "Z") => (7, 3),
+            ("B", "X") => (8, 1),
+            ("B", "Y") => (5, 5),
+            ("B", "Z") => (2, 9),
+            ("C", "X") => (3, 7),
+            ("C", "Y") => (9, 2),
+            ("C", "Z") => (6, 6),
+            _ => todo!(),
+        }
+    }
+    fn get_points_2(&self) -> (i32, i32) {
+        return match self.as_str() {
+            ("A", "Y") => (4, 4),
+            ("A", "Z") => (1, 8),
+            ("A", "X") => (7, 3),
+            ("B", "X") => (8, 1),
+            ("B", "Y") => (5, 5),
+            ("B", "Z") => (2, 9),
+            ("C", "Z") => (3, 7),
+            ("C", "X") => (9, 2),
+            ("C", "Y") => (6, 6),
+            _ => todo!(),
+        }
+    }
+}
+
+pub fn parse_input(file_path: &str) -> Vec<Game> {
     let mut guide: Vec<Game> = vec!();
     
     let mut input = String::new();
@@ -19,49 +52,17 @@ pub fn parse_input(file_path: &str, win_state: bool) -> Vec<Game> {
         }
         let game_vec = game.split(' ').collect::<Vec<&str>>();
         let state = (game_vec[0].to_string(), game_vec[1].to_string());
-        if win_state {
-            guide.push(Game { state, points: calc_points_2(game) })
-        } else {
-            guide.push(Game { state, points: calc_points_1(game) })
-        }
+        guide.push(Game { state })
     }
     guide
 }
 
-fn calc_points_1(game_str: &str) -> (i32, i32) {
-    return match game_str {
-        "A X" => (4, 4),
-        "A Y" => (1, 8),
-        "A Z" => (7, 3),
-        "B X" => (8, 1),
-        "B Y" => (5, 5),
-        "B Z" => (2, 9),
-        "C X" => (3, 7),
-        "C Y" => (9, 2),
-        "C Z" => (6, 6),
-        _ => todo!(),
-    }
-}
-
-fn calc_points_2(game_str: &str) -> (i32, i32) {
-    return match game_str {
-        "A Y" => (4, 4),
-        "A Z" => (1, 8),
-        "A X" => (7, 3),
-        "B X" => (8, 1),
-        "B Y" => (5, 5),
-        "B Z" => (2, 9),
-        "C Z" => (3, 7),
-        "C X" => (9, 2),
-        "C Y" => (6, 6),
-        _ => todo!(),
-    }
-}
-
-pub fn calc_solution(guide: Vec<Game>) -> i32 {
-    let mut score = 0;
+pub fn calc_solution(guide: Vec<Game>) -> (String, String) {
+    let mut score1 = 0;
+    let mut score2 = 0;
     for game in guide {
-        score += game.points.1
+        score1 += game.get_points_1().1;
+        score2 += game.get_points_2().1;
     }
-    return score
+    return (score1.to_string(), score2.to_string())
 }
